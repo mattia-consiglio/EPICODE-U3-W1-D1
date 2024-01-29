48b1c70e26c85127285a851d9cd4567c185284a8
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2024-01-29 14:28:55 +0100

    Added loop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg'
 import './App.css'
 import ButtonComponent from './ButtonComponent'
 import ImageComponent from './ImageComponent'
 
 function App() {
+	const buttonsText = [
+		'Hello World 1',
+		'Hello World 2',
+		'Hello World 3',
+		'Hello World 4',
+		'Hello World 5',
+	]
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<ButtonComponent text='Hello World' />
+				{buttonsText.map(text => (
+					<ButtonComponent key={text} text={text} />
+				))}
+				<hr />
 				<ImageComponent src='https://picsum.photos/200/300' alt='Random image' />
 			</header>
 		</div>
